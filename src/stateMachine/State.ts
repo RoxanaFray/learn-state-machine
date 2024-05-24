b71ab2b5f246f7f9388e1ab3fc29bb733fa52e29
Tanya <roxxxanafray@gmail.com>
2024-05-24 15:25:37 +0300

    implemented abstract state machine

---
+++ src/stateMachine/State.ts
@@ -0,0 +1,5 @@
+export class State {
+  public enter() {}
+  public update() {}
+  public exit() {}
+}
--- src/stateMachine/StateMachine.ts
+++ src/stateMachine/StateMachine.ts
@@ -1,5 +1,19 @@
+import { State } from "./State";
+
 export class StateMachine {
-  public foo() {
-    console.log("HELO!");
+  private currentState: State;
+
+  public setState(state: State) {
+    if (this.currentState) {
+      this.currentState.exit();
+    }
+    this.currentState = state;
+    this.currentState.enter();
+  }
+
+  public update() {
+    if (this.currentState) {
+      this.currentState.update();
+    }
   }
 }
